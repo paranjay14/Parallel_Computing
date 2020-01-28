dc73e7a971e630b334ca7840835a72506b7b983c
paranjay14 <paranjay14@iitg.ernet.in>
2020-01-29 00:52:09 +0530

    Corrected minor errors

--- masterScript.sh
+++ masterScript.sh
@@ -1,14 +1,16 @@
-# bash script.sh 2 1 iter_2_1.txt
-# bash script.sh 4 2 iter_4_2.txt
-# bash script.sh 8 4 iter_8_4.txt
-# bash script.sh 16 8 iter_16_8.txt
-# bash script.sh 32 16 iter_32_16.txt
-# bash script.sh 64 32 iter_64_32.txt
-# bash script.sh 128 32 iter_128_32.txt
-# bash script.sh 256 32 iter_256_32.txt
-# bash script.sh 512 32 iter_512_32.txt
-# bash script.sh 1024 32 iter_1024_32.txt
-# bash script.sh 2048 32 iter_2048_32.txt
-# bash script.sh 4096 32 iter_4096_32.txt
-bash script.sh 8192 32 iter_8192_32.txt
-bash script.sh 8192 64 iter_8192_64.txt
+# bash runScript.sh 2 1 iter_2_1.txt
+# bash runScript.sh 4 2 iter_4_2.txt
+# bash runScript.sh 8 4 iter_8_4.txt
+# bash runScript.sh 16 8 iter_16_8.txt
+# bash runScript.sh 32 16 iter_32_16.txt
+# bash runScript.sh 32 32 iter_32_32.txt
+# bash runScript.sh 64 64 iter_64_64.txt
+# bash runScript.sh 64 32 iter_64_32.txt
+# bash runScript.sh 128 32 iter_128_32.txt
+# bash runScript.sh 256 32 iter_256_32.txt
+# bash runScript.sh 512 32 iter_512_32.txt
+# bash runScript.sh 1024 32 iter_1024_32.txt
+# bash runScript.sh 2048 32 iter_2048_32.txt
+# bash runScript.sh 4096 32 iter_4096_32.txt
+bash runScript.sh 8192 32 iter_8192_32.txt
+bash runScript.sh 8192 64 iter_8192_64.txt
