b881709f44075dc1eb68c59aa09994a2c56f5cf4
paranjay14 <paranjay14@iitg.ernet.in>
2020-02-06 03:36:52 +0530

    Asgn_3 started

---
+++ asgn_3/addNum_Naive.c
@@ -0,0 +1,64 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv)
+{
+    int rank, size, number_amount, N = atoi(argv[1]);
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int x = N/size , tmpSum=0;
+    int *data1 = (int*) malloc(sizeof(int) * N);
+    int *data2 = (int*) malloc(sizeof(int) * x);
+    int *data = (int*) malloc(sizeof(int));
+    double maxTime, timeTaken, timeStart, timeEnd;
+    MPI_Status status;
+
+    if(rank == 0){
+        for (int i = 0; i < N; ++i)
+            data1[i] = 1;
+        
+        for (int i = 1; i < size; ++i)
+            MPI_Send(data1 + i*x,x,MPI_INT,i,0,MPI_COMM_WORLD); 
+    }
+
+    timeStart = MPI_Wtime();
+    if(rank == 0){
+        for (int i = 0; i < x; ++i)
+            tmpSum += data1[i];
+
+        for (int i = 1; i < size; ++i){
+            MPI_Recv(data,1,MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+            tmpSum += data[0];
+        }
+    }
+    else{
+        MPI_Recv(data2,x,MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+        // MPI_Get_count(&status, MPI_INT, &number_amount);
+        // printf("rank=%d received %d numbers from source = %d, tag = %d\n", rank,
+        //    number_amount, status.MPI_SOURCE, status.MPI_TAG);
+
+        for (int i = 0; i < x; ++i)
+            tmpSum += data2[i];
+
+        data[0] = tmpSum;
+        MPI_Send(data,1,MPI_INT,0,0,MPI_COMM_WORLD); 
+    }
+    timeEnd = MPI_Wtime();
+
+    timeTaken = timeEnd-timeStart;
+    MPI_Reduce(&timeTaken,&maxTime,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
+
+    if(rank == 0){
+        // printf("Total Sum = %d\n", tmpSum);
+        // printf("Total Time Taken = ");
+        printf("%f\n", maxTime);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ asgn_3/addNum_Reduce.c
@@ -0,0 +1,52 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv)
+{
+    int rank, size, number_amount, N = atoi(argv[1]);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int x = N/size, tmpSum=0, finalSum=0;
+    int *data1 = (int*) malloc(sizeof(int) * N);
+    int *data2 = (int*) malloc(sizeof(int) * x);
+    int *data = (int*) malloc(sizeof(int) * x);
+    double maxTime, timeTaken, timeStart, timeEnd;
+    MPI_Status status;
+
+    if(rank == 0){
+        for (int i = 0; i < N; ++i)
+            data1[i] = 1;
+        
+        for (int i = 1; i < size; ++i)
+            MPI_Send(data1 + i*x,x,MPI_INT,i,0,MPI_COMM_WORLD); 
+    }
+
+    timeStart = MPI_Wtime();
+    if(rank == 0){
+        for (int i = 0; i < x; ++i)
+            tmpSum += data1[i];
+    }
+    else{
+        MPI_Recv(data2,x,MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        for (int i = 0; i < x; ++i)
+            tmpSum += data2[i];
+    }
+
+    MPI_Reduce(&tmpSum,&finalSum,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
+    timeEnd = MPI_Wtime();
+    timeTaken = timeEnd-timeStart;
+    MPI_Reduce(&timeTaken,&maxTime,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
+
+    if(rank == 0){
+        // printf("Total Sum = %d\n", finalSum);
+        // printf("Total Time Taken = ");
+        printf("%f\n", maxTime);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
