3e4575a20d7a1eca8a4d7298e1410099a27c7cf3
paranjay14 <paranjay14@iitg.ernet.in>
2020-04-18 13:22:18 +0530

    Asgn_7 data files added

--- asgn_7/masterScript.sh
+++ asgn_7/masterScript.sh
@@ -1,16 +1,19 @@
 St=256
-# Fin=524288
-Fin=8192
+Fin=33554432
+# Fin=8192
 
-for (( p=0; p<=5; p=p+1 ))
+for (( p=1; p<=32; p=p*2 ))
 do
 	for (( i=$St; i<=$Fin; i=i*2 ))
 	do  
-	    if [ $i -ge 65536 ]
+	    if [ $i -ge 16777216 ]
 	    then
-	    	bash runScript.sh $p $i iter_${i}_${p}.txt 2
+	    	bash runScript.sh $p $i iterFFT_${i}_${p}.txt 1
+	    elif [ $i -ge 524288 ]
+	    then
+	    	bash runScript.sh $p $i iterFFT_${i}_${p}.txt 2
 	    else
-	    	bash runScript.sh $p $i iter_${i}_${p}.txt 8
+	    	bash runScript.sh $p $i iterFFT_${i}_${p}.txt 8
 	    fi
 	done
 done
--- asgn_7/runScript.sh
+++ asgn_7/runScript.sh
@@ -3,14 +3,13 @@ echo "Value of P : " $1
 echo "Value of N : " $2
 echo "File : " $3
 Iter=$4
-Power2=$((2**$1))
-echo $Power2
+# Power2=$((2**$1))
+# echo $Power2
 
-gcc poly_mul_naive.c -o pmn -lpthread -fopenmp
-gcc poly_mul_krtsb.c -o pmk -lpthread -fopenmp
+gcc poly_mul_fft.c -o pmf -lpthread -fopenmp -lm
 
 s0=0
-s1=0
+# s1=0
 
 > $3
 
@@ -18,20 +17,20 @@ for (( i=1; i<=$Iter; i++ ))
 do  
     echo "Iteration No. : " $i
 
-    t0=`./pmn $Power2 $2`
+    t0=`./pmf $1 $2`
     echo $t0 >> $3
     s0=`echo $s0 + $t0 | bc -l`
 
-    t1=`./pmk $1 $2`
-    echo $t1 >> $3
-    s1=`echo $s1 + $t1 | bc -l`
+    # t1=`./pmk $1 $2`
+    # echo $t1 >> $3
+    # s1=`echo $s1 + $t1 | bc -l`
     
 done
 
 echo " " >> $3
 echo "Mean Values :- " >> $3
 s0=`echo $s0 / $Iter | bc -l  | grep -o '.*[1-9]'`
-s1=`echo $s1 / $Iter | bc -l  | grep -o '.*[1-9]'`
+# s1=`echo $s1 / $Iter | bc -l  | grep -o '.*[1-9]'`
 
 echo $s0 >> $3
-echo $s1 >> $3
+# echo $s1 >> $3
